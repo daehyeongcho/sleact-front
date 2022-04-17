d62b2889394a28e620c397345b0135344c021c14
Randy <dhcho1034@gmail.com>
2022-04-18 00:18:32 +0900

    폴더 구조, 리액트 라우터 세팅

--- client.tsx
+++ client.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './layouts/App'
 
 const container = document.querySelector('#app')
 const root = createRoot(container!)
-root.render(<App />)
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+)
--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import { Navigate } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
+import LogIn from '@pages/Login'
+import SignUp from '@pages/SignUp'
 
-const App = () => <div>좀만 참아주세요!</div>
+const App = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<Navigate replace to='/login' />} />
+            <Route path='/login/*' element={<LogIn />} />
+            <Route path='/signup/*' element={<SignUp />} />
+        </Routes>
+    )
+}
 
 export default App
